3e69a09f57f4543b79d896df440f92bf6f48ae6e
Subin Kim <subinkim325@gmail.com>
2020-07-20 12:57:50 -0700

    New adding feauture

--- src/CodelabsSheet/MainForm.cs
+++ src/CodelabsSheet/MainForm.cs
@@ -23,6 +23,27 @@ namespace CodelabsSheet
             base.OnLoad(e);
 
             exitToolStripMenuItem.Click += ExitToolStripMenuItem_Click;
+            addStripMenuItem1.Click += AddStripMenuItem1_Click;
+        }
+
+        private void AddStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string s1 = spreadsheetControl1.GetCellContents(0, 0);
+            string s2 = spreadsheetControl1.GetCellContents(0, 1);
+            if (s1 == "" || s2 == "")
+            {
+                return;
+            }
+
+            if (int.TryParse(s1, out int v1) && int.TryParse(s2, out int v2))
+            {
+                int sum = v1 + v2;
+                spreadsheetControl1.SetCellContents(0, 2, sum.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Error Only Integers Allowed");
+            }
         }
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
