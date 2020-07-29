4a22f41606b4d4bcca39208ccd1963d8c76fd356
William Kent <wjk@users.noreply.github.com>
2020-07-29 14:31:24 -0400

    Fix control overlap on high-DPI monitors

--- src/CodelabsSheet.Controls/AboutForm.Designer.cs
+++ src/CodelabsSheet.Controls/AboutForm.Designer.cs
@@ -41,9 +41,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(-134, -28);
+            this.label1.Location = new System.Drawing.Point(-249, -60);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(78, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -51,53 +52,59 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(-163, -2);
+            this.label2.Location = new System.Drawing.Point(-303, -4);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.Size = new System.Drawing.Size(78, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(500, 29);
+            this.label3.Location = new System.Drawing.Point(929, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.Size = new System.Drawing.Size(149, 32);
             this.label3.TabIndex = 3;
             this.label3.Text = "William Kent";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(500, 44);
+            this.label4.Location = new System.Drawing.Point(929, 94);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 15);
+            this.label4.Size = new System.Drawing.Size(123, 32);
             this.label4.TabIndex = 4;
             this.label4.Text = "Subin Kim";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(500, 59);
+            this.label5.Location = new System.Drawing.Point(929, 126);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 15);
+            this.label5.Size = new System.Drawing.Size(143, 32);
             this.label5.TabIndex = 5;
             this.label5.Text = "Veny Wijaya";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(437, 9);
+            this.label6.Location = new System.Drawing.Point(929, 19);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 15);
+            this.label6.Size = new System.Drawing.Size(130, 32);
             this.label6.TabIndex = 6;
             this.label6.Text = "Created by";
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(606, 103);
+            this.btnClose.Location = new System.Drawing.Point(1125, 220);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(139, 49);
             this.btnClose.TabIndex = 7;
             this.btnClose.Text = "OK";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -107,17 +114,18 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(-9, 9);
+            this.label7.Location = new System.Drawing.Point(-17, 19);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(440, 81);
+            this.label7.Size = new System.Drawing.Size(883, 159);
             this.label7.TabIndex = 8;
             this.label7.Text = "CodeLabsSheet";
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(719, 151);
+            this.ClientSize = new System.Drawing.Size(1335, 322);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.label6);
@@ -127,6 +135,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
